f5f22c52328e495745d274881ef3dba6f0d362ac
Augusto Vicente <augustovicente201831464@cunoc.edu.gt>
2021-06-09 11:20:03 -0600

    Crear Ejercicio2.java, de los algoritmos anteriores

---
+++ Ejercicio2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio2{
+
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+		String pais;
+		String capital;
+		
+		System.out.print("Ingrese el pais: ");
+		pais = entrada.nextLine();
+		
+		System.out.print("Ingrese la capital del pais: ");
+		capital = entrada.nextLine();		
+		
+		System.out.println(capital + " es la capital de " + pais);		
+	}
+}
